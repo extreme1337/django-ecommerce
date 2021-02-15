67ac9f8af6322d71012c590952fa7acaf556f88c
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-02-15 14:17:53 +0100

    fixed issue with deleting items from cart

--- core/views.py
+++ core/views.py
@@ -61,7 +61,7 @@ def remove_from_cart(request, slug):
     )
     if order_qs.exists():
         order = order_qs[0]
-        
+        # check if the order item is in the order
         if order.items.filter(item__slug=item.slug).exists():
             order_item = OrderItem.objects.filter(
                 item=item,
@@ -69,11 +69,13 @@ def remove_from_cart(request, slug):
                 ordered=False
             )[0]
             order.items.remove(order_item)
-            messages.info(request, "This item was removed from your cart")
+            order_item.delete()
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("core:product", slug=slug)
         else:
             messages.info(request, "This item was not in your cart")
             return redirect("core:product", slug=slug)
     else:
-        messages.info(request, "You do not have an actie order")
+        messages.info(request, "You do not have an active order")
         return redirect("core:product", slug=slug)
     
